a74dfe44b7e3cde1be86a6673dcb40239c9260bf
Erick <erick.personal16@outlook.com>
2024-09-10 15:34:54 +0200

    java: actualizacion

---
+++ Java-Netbeans/TP2/src/Prueba/Arraylist.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Prueba;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Santy
+ */
+public class Arraylist extends ArrayList<String> {
+
+    public Arraylist() {
+    }
+    
+}
--- Java-Netbeans/TP2/src/Prueba/Principal.java
+++ Java-Netbeans/TP2/src/Prueba/Principal.java
@@ -6,38 +6,43 @@ import java.util.Scanner;
 public class Principal {
 
     public static void main(String[] args) {
-
-        Scanner entrada = new Scanner(System.in);
-        int longitudContraseña = leerLongitudContraseña(entrada);
-        boolean mayusculas = verificacionDeOpciones(entrada, "mayusculas");
-        boolean minusculas = verificacionDeOpciones(entrada, "minusculas");
-        boolean numeros = verificacionDeOpciones(entrada, "numeros");
-        boolean caracteresEspeciales = verificacionDeOpciones(entrada, "caracteres especiales");
-
-        String abecedario = "abcdefghijklmnopqrstuvwxyz";
-        String abecedarioMayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String listaNumeros = "123456789";
-        String especiales = "({)[}|@·#]";
-
-        String clave = new String();
-        for (int i = 0; i < longitudContraseña; i++) {
-            String caracteres = "";
-            if (mayusculas) {
-                caracteres += abecedarioMayusculas;
-            }
-            if (minusculas) {
-                caracteres += abecedario;
-            }
-            if (numeros) {
-                caracteres += listaNumeros;
-            }
-            if (caracteresEspeciales) {
-                caracteres += especiales;
-            }
-            clave += caracteres.charAt( (int) (Math.random() * caracteres.length() ) );
-        }
-
-        System.out.println(clave);
+        int numeros[] = new int[5];
+        numeros[0] = 10;
+        numeros[1] = 10;
+        numeros[2] = 10;
+        numeros[3] = 10;
+        System.out.println(numeros.length);
+//        Scanner entrada = new Scanner(System.in);
+//        int longitudContraseña = leerLongitudContraseña(entrada);
+//        boolean mayusculas = verificacionDeOpciones(entrada, "mayusculas");
+//        boolean minusculas = verificacionDeOpciones(entrada, "minusculas");
+//        boolean numeros = verificacionDeOpciones(entrada, "numeros");
+//        boolean caracteresEspeciales = verificacionDeOpciones(entrada, "caracteres especiales");
+//
+//        String abecedario = "abcdefghijklmnopqrstuvwxyz";
+//        String abecedarioMayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//        String listaNumeros = "123456789";
+//        String especiales = "({)[}|@·#]";
+//
+//        String clave = new String();
+//        for (int i = 0; i < longitudContraseña; i++) {
+//            String caracteres = "";
+//            if (mayusculas) {
+//                caracteres += abecedarioMayusculas;
+//            }
+//            if (minusculas) {
+//                caracteres += abecedario;
+//            }
+//            if (numeros) {
+//                caracteres += listaNumeros;
+//            }
+//            if (caracteresEspeciales) {
+//                caracteres += especiales;
+//            }
+//            clave += caracteres.charAt( (int) (Math.random() * caracteres.length() ) );
+//        }
+//
+//        System.out.println(clave);
     }
 
     public static int leerLongitudContraseña(Scanner entrada) {
