99a0b4a12299f10b5000b541ca8bf8ddc6d79186
Erick <erick.personal16@outlook.com>
2024-09-15 18:08:41 +0200

    Nada

--- Java-Netbeans/TP03/src/ClaseGenerica/PrincipalConGenerico.java
+++ Java-Netbeans/TP03/src/ClaseGenerica/PrincipalConGenerico.java
@@ -5,21 +5,6 @@ import java.util.ArrayList;
 public class PrincipalConGenerico {
 
     public static void main(String[] args) {
-        
-        ArrayList<Integer> numerosEnteros = new ArrayList<>();
-        
-        numerosEnteros.add(10);
-        numerosEnteros.add(24);
-        numerosEnteros.add(3);
-        numerosEnteros.add(2);
-        
-        Operaciones.mostrar(numerosEnteros);
-        
-        ClaseGenerica<Integer> variableEntera = new ClaseGenerica(10);
-        System.out.println(variableEntera.getValor());
-        
-        System.out.println(Operaciones.sumar(variableEntera.getValor(), 4.4));
-        
-        System.out.println(String.valueOf(1000));
+        System.out.println("hola");
     }
 }
